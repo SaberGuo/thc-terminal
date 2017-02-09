47b482a7473883468a885585cae6ee9fafe0da55
guoxiao <saber.guo@gmail.com>
2017-02-09 12:49:48 +0000

    modify the ftp server to capture the img by alarm

--- ftp_server/app.py
+++ ftp_server/app.py
@@ -15,9 +15,12 @@ if __name__ == "__main__":
     net_power_ctrl("on")
     timer_proc(200)
     net_reset()
-
+    camera_power_ctrl("on")
+    timer_proc(40000)
+    alarm_on()
     os.system("sudo ./build/ftp_server")
-
+    alarm_off()
+    camera_power_ctrl("off")
     net_power_ctrl("off")
     setdown_driver()
 
